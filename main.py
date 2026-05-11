cc7761871db30c36d39545d14adc915315bccdea
HappyhireLaurent <laurent@happy-hire.com>
2026-05-11 14:14:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def get_token() -> str:
             "grant_type":    "client_credentials",
             "client_id":     FT_CLIENT_ID,
             "client_secret": FT_CLIENT_SECRET,
-            "scope":         "Open_Formation_v1",
+            "scope":         "Open Formation",
         },
         timeout=15,
     )
